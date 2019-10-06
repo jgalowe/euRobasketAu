9a864838b320d97923970b8fd34884f6628430b3
jgalowe <45960266+jgalowe@users.noreply.github.com>
2019-10-06 14:14:39 +1100

    Update get_stints_fibalivestats.R

--- R/get_stints_fibalivestats.R
+++ R/get_stints_fibalivestats.R
@@ -164,12 +164,12 @@ get_stint_data = function(start_row, end_row) {
   home_pts = (home_2pt_fgm*2) + (home_3pt_fgm*3) + home_ftm
    
   #turnover types
-  home_ballhandling = home_actions[home_actions$subType ==  "ballhandling"|home_actions$subType == "doubledribble"|home_actions$subType == "travel",]
-  home_badpass = home_actions[home_actions$subType ==  "badpass",]
-  home_oFoul = home_actions[home_actions$subType ==  "offensive",] 
-  home_3sec = home_actions[home_actions$subType ==  "3sec",]
-  home_8sec = home_actions[home_actions$subType ==  "8sec",]
-  home_24sec = home_actions[home_actions$subType ==  "24sec",]
+  home_ballhandling = nrow(home_actions[which(home_actions$subType ==  "ballhandling"|home_actions$subType == "doubledribble"|home_actions$subType == "travel"),]
+  home_badpass = nrow(home_actions[which(home_actions$subType ==  "badpass"),]
+  home_oFoul = nrow(home_actions[which(home_actions$subType ==  "offensive",] 
+  home_3sec = nrow(home_actions[which(home_actions$subType ==  "3sec"),]
+  home_8sec = nrow(home_actions[which(home_actions$subType ==  "8sec"),]
+  home_24sec = nrow(home_actions[which(home_actions$subType ==  "24sec",]
    
   #create data.frame with actions
   home = data.frame(cbind(
